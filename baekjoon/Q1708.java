b36d027bb6bfc53f21efc38c9662c1c3ca5bad4c
chulman <chlcjfals0122@gmail.com>
2019-07-05 09:49:13 +0900

    convex hull complete.

--- baekjoon/Q1708.java
+++ baekjoon/Q1708.java
@@ -1,20 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q1708 {
 
     public static class Point {
-        int x;
-        int y;
+        long x;
+        long y;
 
-        public Point(int x, int y) {
+        public Point(long x, long y) {
             this.x = x;
             this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
         }
     }
 
@@ -32,22 +37,26 @@ public class Q1708 {
             arr[i] = new Point(x, y);
         }
 
-        Arrays.sort(arr, Comparator.comparingInt(value -> value.y));
-
-        Arrays.sort(arr, 1, arr.length, (o1, o2) -> ccw(arr[0], o1, o2));
-
+        Arrays.sort(arr, Comparator.comparingLong((Point o) -> o.y).thenComparingLong((Point o) -> -o.x));
+        Arrays.sort(arr, 1, arr.length, (o1, o2) -> ccw(arr[0],o2,o1));
 
         Stack<Point> stack = new Stack();
-        stack.add(arr[0]);
-
+        stack.push(arr[0]);
         for (int i = 1; i < arr.length; i++) {
             if (stack.size() < 2) {
                 stack.push(arr[i]);
                 continue;
             }
-            if(ccw(stack.peek(), stack.get(stack.size()-2) , arr[i]) <= 0){
-                stack.pop();
+           Point top = stack.pop();
+
+            while(ccw(stack.peek(), top, arr[i])<= 0){
+                top = stack.pop();
+
+                if(stack.isEmpty()){
+                    break;
+                }
             }
+            stack.push(top);
             stack.push(arr[i]);
         }
         System.out.println(stack.size());
@@ -55,10 +64,10 @@ public class Q1708 {
 
 
     private static int ccw(Point A, Point B, Point C) {
-        int ccw = (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y);
+        long ccw = (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y);
 
         if (ccw > 0) return 1;
-        if (ccw < 0) return -1;
-        return 0;
+        else if (ccw < 0) return -1;
+        else return 0;
     }
 }
