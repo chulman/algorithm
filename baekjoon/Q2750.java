060ec56a2ab9738632408aaf3d14ac81563f0851
chulman <chlcjfals0122@gmail.com>
2019-07-22 20:28:11 +0900

    Q2750 add & complete

--- baekjoon/Q2750.java
+++ baekjoon/Q2750.java
@@ -14,22 +14,42 @@ public class Q2750 {
         for (int i=0; i<N; i++) {
             arr[i] = Integer.valueOf(reader.readLine());
         }
-        bubbleSort(arr, arr.length);
+        bubbleSort(arr);
+//        selectionSort(arr);
+//        insertSort(arr);
         Arrays.stream(arr).forEach(System.out::println);
 
     }
-    //TODO
-    public static void selectionSort(){
 
+    // 최소 값을 선택해서 각 인덱스에 채워 넣음
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[temp] > arr[j]) {
+                    temp = j;
+                }
+            }
+            swap(arr, i, temp);
+        }
     }
-    //TODO
-    public static void insertSort(){
+    // 두번째 인덱스부터 시작해서 작은 값이 있을 때마다 앞 인덱스에 채워넣고 밀어넣음.
+    public static void insertSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int aux = i - 1;
 
+            while ((aux >= 0) && (arr[aux] > temp)) {
+                arr[aux + 1] = arr[aux];
+                aux--;
+            }
+            arr[aux + 1] = temp;
+        }
     }
 
-    public static void bubbleSort(int[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     swap(arr, i, j);
                 }
