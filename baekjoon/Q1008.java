f940bb857782813c4c248be24745009ece3afb4e
chulman <chlcjfals0122@gmail.com>
2019-07-25 21:39:37 +0900

    Q1008 add

---
+++ baekjoon/Q1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+
+        double d = a/b;
+        System.out.println(d);
+
+    }
+}
