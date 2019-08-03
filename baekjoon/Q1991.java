e37f639a6e9d553e83cde149be35a9ea8a58fc79
chulman <chlcjfals0122@gmail.com>
2019-08-03 12:21:06 +0900

    Q1991 commit

---
+++ baekjoon/Q1991.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1991 {
+    private static int line = 5;
+    private static int count = 4;
+    private static int[][] cookScore = new int[line][count];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < line; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < count; j++) {
+                cookScore[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
+        }
+        reader.close();
+        calcMaxScore();
+
+    }
+
+    private static void calcMaxScore() {
+
+        int index = -1;
+        int max = 0;
+
+        for (int i = 0; i < cookScore.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < cookScore[i].length; j++)
+                sum += cookScore[i][j];
+            if (max < sum) {
+                index = i;
+                max = sum;
+            }
+        }
+
+        System.out.println((index+1) + " " + max);
+    }
+}
