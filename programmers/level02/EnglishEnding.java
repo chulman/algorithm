572baa2e0f7bb1bd18c58f8dba134af8ade91c6d
chulman <chlcjfals0122@gmail.com>
2019-08-21 23:29:34 +0900

    level2 - 영어 끝말잇기 수정

--- programmers/level02/EnglishEnding.java
+++ programmers/level02/EnglishEnding.java
@@ -36,7 +36,10 @@ public class EnglishEnding {
 
     private int[] answer(int n, int count, int i) {
         int[] answer = new int[2];
+        if((i+1)%n == 0) answer[0] = n;
+        else answer[0] = (i+1)%n;
 
+        answer[1] = count;
         return answer;
     }
 }
