8313fc21fab30ecf5ecd738aab67e81436bd04bf
chulman <chlcjfals0122@gmail.com>
2019-09-05 23:58:13 +0900

    가장 큰 수 찾기

---
+++ programmers/level02/LarggerNumber.java
@@ -0,0 +1,23 @@
+package level02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LarggerNumber {
+    public String solution(int[] numbers) {
+        String answer = "";
+        List<Integer> list = new ArrayList<>();
+
+        int pos = 0;
+        for(int i=0; i<numbers.length; i++){
+            for(int j=0; j<numbers.length; j++){
+                if(j!=pos){
+
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
