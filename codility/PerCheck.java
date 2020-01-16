cb1bea900ebc6b516da51e6b7445e4f9acca34b9
chulman <chlcjfals0122@gmail.com>
2020-01-17 00:11:12 +0900

    PerCheck, PermMissingElem

---
+++ codility/PerCheck.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class PerCheck {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
